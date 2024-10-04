7153becaad258384f97a25bd32161c803f9d7268
Riya <riyavk026@gmail.com>
2024-10-04 12:26:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("btn")
+
+// button.addEventListener("click", ()=>{
+    // alert("i was clicked.yayyy!!!")
+// })
+
+button.addEventListener("dblclick", ()=>{
+    document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
+})
